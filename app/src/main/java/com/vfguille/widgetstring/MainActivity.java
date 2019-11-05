ed530937428b1680f323d37d7b02e3a8d73355e5
Guillermo Vera <vfguille@gmail.com>
2019-11-05 12:05:42 +0100

    Interfaz

---
+++ app/src/main/java/com/vfguille/widgetstring/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vfguille.widgetstring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
