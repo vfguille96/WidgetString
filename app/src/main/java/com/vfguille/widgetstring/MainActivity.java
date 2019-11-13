2f51616c5a32a8e2151d0e7cdeb975081196cff6
Guillermo Vera <vfguille@gmail.com>
2019-11-13 09:49:23 +0100

    Ej. terminado

--- app/src/main/java/com/vfguille/widgetstring/MainActivity.java
+++ app/src/main/java/com/vfguille/widgetstring/MainActivity.java
@@ -9,6 +9,7 @@ import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -20,32 +21,66 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spNumEmpl;
     private AutoCompleteTextView autoCompleteTextView;
     private WebView webView;
+    private RadioGroup radioGroup;
+    private View inclEmpr;
+    private View inclPart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
-        webView = findViewById(R.id.wvBussiness);
-        spNumEmpl = findViewById(R.id.spinner);
+        InitializeComponents();
         initializeActMonth();
         initializeWebView();
         initializeSpinner();
+        initializeRgTypeClient();
+    }
+
+    private void InitializeComponents() {
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        webView = findViewById(R.id.wvBussiness);
+        spNumEmpl = findViewById(R.id.spinner);
+        radioGroup = findViewById(R.id.radioGroup);
+        inclEmpr = findViewById(R.id.inclBus);
+        inclPart = findViewById(R.id.inclPar);
+    }
+
+    /*
+    Realiza una acción cuando se selecciona un Radio Button.
+     */
+    private void initializeRgTypeClient() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.rbParticular:
+                        inclEmpr.setVisibility(View.GONE);
+                        inclPart.setVisibility(View.VISIBLE);
+                        break;
+                    case R.id.rbEmpresa:
+                        inclEmpr.setVisibility(View.VISIBLE);
+                        inclPart.setVisibility(View.GONE);
+                        break;
+                }
+            }
+        });
     }
 
     private void initializeSpinner() {
         spNumEmpl.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                //Toast.makeText(adapterView.getContext(), "Seleccionado: " + spNumEmpl.getItemAtPosition(i).toString() + " empleado", Toast.LENGTH_LONG).show();
-                String message = spNumEmpl.getResources().getQuantityString(R.plurals.num_empl_pl, Integer.parseInt(spNumEmpl.getSelectedItem().toString()));
+                String message;
+                if (spNumEmpl.getSelectedItem().equals("0"))
+                    message = getString(R.string.zeroE);
+                    //Toast.makeText(adapterView.getContext(), "Seleccionado: " + spNumEmpl.getItemAtPosition(i).toString() + " empleado", Toast.LENGTH_LONG).show();
+                else
+                    message = spNumEmpl.getResources().getQuantityString(R.plurals.num_empl_pl, Integer.parseInt(spNumEmpl.getSelectedItem().toString()), Integer.parseInt(spNumEmpl.getSelectedItem().toString()));
                 Toast.makeText(adapterView.getContext(), message, Toast.LENGTH_LONG).show();
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
+            public void onNothingSelected(AdapterView<?> adapterView) { }
         });
     }
 
@@ -58,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void initializeActMonth() {
         String[] months = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,  months);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, months);
         autoCompleteTextView.setAdapter(arrayAdapter);
     }
 }
